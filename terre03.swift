5051fbc4a2c27fd327839c28ba08cbac6f9ff12f
croissance <myadressemailisthat@gmail.com>
2022-01-13 23:31:32 +0100

    recherche en cours

--- terre03.swift
+++ terre03.swift
@@ -5,11 +5,14 @@ let numberOfArguments = argument.count - 1
 let numberOfArrayElements = alphabetLetters.count - 1
 
 
-for i in alphabetLetters {
-    if numberOfArguments != 1 || argument[1] != i {
-        print ("Vous devez fournir une lettre minuscule des 26 lettres de base de l'alphabet, rien de plus, rien de moins")
-    } else {
-        print (argument[1])
-    }
+i = alphabetLetters[0]
+
+if numberOfArguments != 1 || argument[1] != i {
+    print ("Vous devez fournir une lettre minuscule des 26 lettres de base de l'alphabet, rien de plus, rien de moins")
+} else {
+    print ("Yes")
 }
 
+
+
+// Ce que l'on veut, on veut que ma boucle parcours le tableau jusqu'à ce qu'il trouve argument[1] = i. Une fois cela trouvé, il faut qu'il affiche tous les éléments qu'ils parcourent
