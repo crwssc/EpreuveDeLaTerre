bdefd3ee74ce753b831ae8a3b81332d7c77f3a11
croissance <myadressemailisthat@gmail.com>
2022-02-01 08:24:48 +0100

    j'ai l'air proche

--- terre03.swift
+++ terre03.swift
@@ -4,25 +4,6 @@ var argument = CommandLine.arguments
 let numberOfArguments = argument.count - 1
 let numberOfArrayElements = alphabetLetters.count - 1
 
-/*
-if numberOfArguments != 1 {
-    print ("Vous devez fournir un seul argument")
-} else {
-    for i in alphabetLetters {
-        if (argument[1] == i) {
-            var myIndex = alphabetLetters.firstIndex(of: i)!
-            while (myIndex < 26) {
-                print (alphabetLetters[myIndex], terminator:"") ;
-                myIndex += 1
-            }
-            print()
-        } else {
-            print ("Vous devez fournir une lettre de l'alphabet")
-        }
-    }
-}
-*/
-
 if numberOfArguments != 1 {
     print ("Vous devez fournir un seul argument")
 } else {
@@ -30,19 +11,12 @@ if numberOfArguments != 1 {
     var theIndex = 0
     while (theIndex < 26) {
         if testingArg == alphabetLetters[theIndex] {
-            for i in alphabetLetters {
-                var myIndex = alphabetLetters.firstIndex(of: i)!
-                while (myIndex < 26) {
-                    print (alphabetLetters[myIndex], terminator:"") ;
-                    myIndex += 1
-                }
-                print()
+            for _ in alphabetLetters {
+                print (alphabetLetters[theIndex])//, terminator:"")
+                theIndex += 1
             }
-            break
         } else {
             theIndex += 1
         }
     }
 }
-
-
