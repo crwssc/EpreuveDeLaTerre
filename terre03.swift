ca40c5d4678fb8a7b1147eb0128c448c45a78491
croissance <myadressemailisthat@gmail.com>
2022-02-02 10:19:03 +0100

    il me reste à gérer le cas de la lettre z

--- terre03.swift
+++ terre03.swift
@@ -12,19 +12,19 @@ var letterIndex = 0
 if numberOfArguments != 1 {
     print ("Vous devez fournir un argument, pas deux, pas zéro, mais bien 1")
 } else {
-    repeat {
+    while (letterIndex <= numberOfArrayElements) {
         if (arg == alphabetLetters[letterIndex]) {
-            for i in alphabetLetters {
-                print (i, terminator:"")
+            while (letterIndex <= numberOfArrayElements) {
+                print (alphabetLetters[letterIndex], terminator:"")
+                letterIndex += 1
             }
             print()
-            letterIndex += 1
         } else {
             letterIndex += 1
             if letterIndex == numberOfArrayElements {
                 print("Vous devez fournir une lettre de l'alphabet")
             }
         }
-    } while (letterIndex <= numberOfArrayElements)
+    }
 }
 
