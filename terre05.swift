f93412d42f556ad143b7968051499f6820649423
croissance <myadressemailisthat@gmail.com>
2022-02-21 15:07:08 +0100

    histoire des conditions réglée

--- terre05.swift
+++ terre05.swift
@@ -3,9 +3,9 @@ argument.remove(at:0)
 var dividende = argument[0]
 var diviseur = argument[1]
 
-if argument.count == 2 && dividende >= diviseur &&  {
-    print ("résultat: \()")
-    print ("reste: \()")
+if (argument.count == 2 && dividende >= diviseur) && (Int(dividende) != nil && Int(diviseur) != nil) {
+    print ("résultat: \(dividende)")
+    print ("reste: \(diviseur)")
 } else {
     print ("erreur, soit t'as pas mis le bon nombre d'arguments, soit t'as pas mis des nombres entiers positifs, soit le premier nombre est inférieur au second")
 }
