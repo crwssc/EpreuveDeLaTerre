af09e14c1492cababcd7d80be006d974e9e2282f
croissance <myadressemailisthat@gmail.com>
2022-02-21 15:20:13 +0100

    il ne reste plus qu'a s'occuper du reste (magnifique jeu de mot)

--- terre05.swift
+++ terre05.swift
@@ -3,9 +3,12 @@ argument.remove(at:0)
 var dividende = argument[0]
 var diviseur = argument[1]
 
-if (argument.count == 2 && dividende >= diviseur) && (Int(dividende) != nil && Int(diviseur) != nil) {
-    print ("résultat: \(dividende)")
+if (argument.count == 2 && Int(dividende)! >= Int(diviseur)!) && (Int(dividende) != nil && Int(diviseur) != nil) {
+    let intDividende = Int(dividende)!
+    let intDiviseur = Int(diviseur)!
+    print ("résultat: \(intDividende/intDiviseur)")
     print ("reste: \(diviseur)")
 } else {
-    print ("erreur, soit t'as pas mis le bon nombre d'arguments, soit t'as pas mis des nombres entiers positifs, soit le premier nombre est inférieur au second")
+    print ("erreur, soit t'as pas mis le bon nombre d'arguments, soit t'as pas mis des nombres entiers positifs, soit le premier nombre est inférieur au second, corrige ça")
 }
+
