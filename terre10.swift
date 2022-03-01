2992ac82b2a4d0c9b65ae61c60fd811a41cb3c44
croissance <myadressemailisthat@gmail.com>
2022-03-01 09:46:11 +0100

    nombres premiers ça avance

--- terre10.swift
+++ terre10.swift
@@ -1,9 +1,19 @@
 var argument = CommandLine.arguments
 argument.remove(at:0)
+var theArgument = argument[0]
 
-if argument.count != 1 {
-    // on gère les erreurs
+if argument.count != 1 || Int(theArgument) == nil {
+    print ("tu dois mettre un nombre entier, pas trois lettre, pas rien, un nombre entier")
 } else {
-    let floatArgument = Float(argument[0])!
-    print (floatArgument.squareRoot())
+    var floatArgument = Float(argument[0])!
+    var squareRootArg = floatArgument.squareRoot()
+    var theNearestWholeNumberOfTheSquareRootArg = Int(squareRootArg)
+    for i in 2...theNearestWholeNumberOfTheSquareRootArg {
+        var resultat = floatArgument / i
+        var intResultat = Int(resultat)
+        var floatResultat = Float(intResultat)
+        if floatResultat == resultat {
+            
+        }
+    }
 }
