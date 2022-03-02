c08821a39870db62672b1daacc3f05e3a1803e46
croissance <myadressemailisthat@gmail.com>
2022-03-02 12:46:04 +0100

    on avance

--- terre13.swift
+++ terre13.swift
@@ -1,3 +1,9 @@
 // Trouver la Suisse (lol)
 
-var argument 
+var argument = CommandLine.arguments
+
+if argument.count != 3 {
+    print ("Vous devez fournir exactement trois nombres entiers")
+} else if {
+    
+}
