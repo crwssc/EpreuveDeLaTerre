38c2ed4d1a1f2f1008c920ac4419e462f3fd5af3
croissance <myadressemailisthat@gmail.com>
2022-03-02 11:45:30 +0100

    on avance

--- terre11.swift
+++ terre11.swift
@@ -5,5 +5,21 @@ var heure = argument[0]
 var colons = argument[1]
 var minute = argument[2]
 
-print("\(argument[0]) : \(argument[2])AM")
-print("\(argument[0]) : \(argument[2])PM")
+if argument.count != 3 {
+    print ("Vous devez fournir l'heure en respectant cette forme : 23 : 43")
+} else if ((Int(heure) == nil) || (Int(minute == nil)) {
+    print ("Vous devez fournir l'heure en respectant cette forme : 23 : 43")
+} else if heure > 24 || minute > 60 {
+    print ("Il n'y a que 24 heure dans la journée et que 60 minutes dans l'heure")
+} else {
+    switch heure {
+    case 00:
+        print("\(12) : \(minute)PM")
+    case 12:
+        print("\(12) : \(minute)PM")
+    case 1...11:
+        print("\(heure) : \(minute)AM")
+    case 13...23:
+        print("\() : \(minute)PM")
+    }
+}
