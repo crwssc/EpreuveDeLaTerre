6c5e95e0db2f437abbb1f0ec70ae209806a28081
croissance <myadressemailisthat@gmail.com>
2022-03-02 10:10:40 +0100

    certaines erreurs gérées

--- terre10.swift
+++ terre10.swift
@@ -2,10 +2,10 @@ var argument = CommandLine.arguments
 argument.remove(at:0)
 var theArgument = argument[0]
 
-if (argument.count != 1 || Int(theArgument)! == nil) {
+if (argument.count != 1 || Int(theArgument) == nil) {
     print ("tu dois mettre un nombre entier, pas trois lettre, pas rien, un nombre entier")
 } else if (Int(theArgument)! < 2) {
-    print ("\(theArgument) n'est pas un nombre premier")
+    print ("\(theArgument) n'est pas un nombre premier, ni un nombre second d'ailleurs mdrr. Qu'est ce qu'on rigole haha")
 } else {
     let floatArgument = Float(argument[0])!
     let squareRootArg = floatArgument.squareRoot()
