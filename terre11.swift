4740c01baa5a0912f918fa4d36ad6d70da16caaf
croissance <myadressemailisthat@gmail.com>
2022-03-02 11:57:55 +0100

    adding file

--- terre11.swift
+++ terre11.swift
@@ -6,9 +6,9 @@ var colons = argument[1]
 var minute = argument[2]
 
 if argument.count != 3 {
-    print ("Vous devez fournir l'heure en respectant cette forme : 23 : 43")
-} else if ((Int(heure) == nil) || (Int(minute == nil)) {
-    print ("Vous devez fournir l'heure en respectant cette forme : 23 : 43")
+    print ("Vous devez fournir l'heure en respectant cette forme : 23 : 43 (mettre des espaces entre chaque élément)")
+} else if ((Int(heure)! == nil) || (Int(minute)! == nil)) {
+    print ("Vous devez fournir l'heure en respectant cette forme : 23 : 43  (mettre des espaces entre chaque élément)")
 } else if heure > 24 || minute > 60 {
     print ("Il n'y a que 24 heure dans la journée et que 60 minutes dans l'heure")
 } else {
@@ -16,10 +16,10 @@ if argument.count != 3 {
     case 00:
         print("\(12) : \(minute)PM")
     case 12:
-        print("\(12) : \(minute)PM")
+        print("\(12) : \(minute)AM")
     case 1...11:
         print("\(heure) : \(minute)AM")
     case 13...23:
-        print("\() : \(minute)PM")
+        print("\(heure - 12) : \(minute)PM")
     }
 }
