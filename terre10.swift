d44f9421e4233980e6a73c2f2a22904e22470ec9
croissance <myadressemailisthat@gmail.com>
2022-03-02 09:49:21 +0100

    un petit commit

--- terre10.swift
+++ terre10.swift
@@ -2,18 +2,26 @@ var argument = CommandLine.arguments
 argument.remove(at:0)
 var theArgument = argument[0]
 
-if argument.count != 1 || Int(theArgument) == nil {
+if (argument.count != 1 || Int(theArgument)! == nil) {
     print ("tu dois mettre un nombre entier, pas trois lettre, pas rien, un nombre entier")
+} else if (Int(theArgument)! < 2) {
+    print ("\(theArgument) n'est pas un nombre premier")
 } else {
-    var floatArgument = Float(argument[0])!
-    var squareRootArg = floatArgument.squareRoot()
-    var theNearestWholeNumberOfTheSquareRootArg = Int(squareRootArg)
-    for i in 2...theNearestWholeNumberOfTheSquareRootArg {
-        var resultat = floatArgument / i
-        var intResultat = Int(resultat)
-        var floatResultat = Float(intResultat)
-        if floatResultat == resultat {
-            
+    let floatArgument = Float(argument[0])!
+    let squareRootArg = floatArgument.squareRoot()
+    let theNearestWholeNumberOfTheSquareRootArg = Int(squareRootArg)
+    var theParameter : Float = 0
+    while theNearestWholeNumberOfTheSquareRootArg > Int(theParameter) {
+        let possibleDivider = floatArgument / theParameter
+        let intPossibleDivider = Int(possibleDivider)
+        let floatPossibleDivider = Float(intPossibleDivider)
+        if (floatPossibleDivider == possibleDivider) {
+            print ("\(theArgument) est un nombre premier")
+            break
+        } else if (Int(theParameter) == theNearestWholeNumberOfTheSquareRootArg) {
+            print ("\(theArgument) n'est pas un nombre premier")
+        } else {
+            theParameter += 1
         }
     }
 }
