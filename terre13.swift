7cd78971e2daa02e0743889f7dc6cde7c6ca3db8
croissance <myadressemailisthat@gmail.com>
2022-03-02 12:58:57 +0100

    petit à petit

--- terre13.swift
+++ terre13.swift
@@ -1,9 +1,10 @@
 // Trouver la Suisse (lol)
 
 var argument = CommandLine.arguments
+argument.remove(at:0)
 
 if argument.count != 3 {
     print ("Vous devez fournir exactement trois nombres entiers")
-} else if {
-    
+} else if Int(argument[0]) == nil || Int(argument[1]) == nil || Int(argument[0] == nil ) {
+    print ("Vous devez fournir des nombres entiers")
 }
--- terre14.swift
+++ terre14.swift
@@ -1,1 +1,10 @@
 // Trié ou pas
+
+var argument = CommandLine.arguments
+argument.remove(at:0)
+
+if argument.count < 1 {
+    print ("Vous devez fournir au moins un argument")
+} else {
+    
+}
