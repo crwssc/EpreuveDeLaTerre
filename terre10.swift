b9b732cfcb50ac063e18fc7c7a9aefb8fa55ba7f
croissance <myadressemailisthat@gmail.com>
2022-03-18 08:25:53 +0100

    nombre premier résolu

--- terre10.swift
+++ terre10.swift
@@ -9,7 +9,17 @@ if (argument.count != 1 || Int(theArgument) == nil) {
 } else if (Int(theArgument)! < 2) {
     print ("\(theArgument) n'est pas un nombre premier, ni un nombre second d'ailleurs mdrr. Qu'est ce qu'on rigole haha")
 } else {
-    
+    let theIntArgument = Int(theArgument)!
+    var i = 2
+    while i < theIntArgument {
+        if theIntArgument % i == 0 {
+            print ("le nombre n'est pas premier")
+            break
+        } else {
+            i += 1
+            if i == theIntArgument - 1 {
+                print ("le nombre est premier")
+            }
         }
     }
 }
