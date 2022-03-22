d04dfbcc56a6bec074175be6d9696b88df85ebae
croissance <myadressemailisthat@gmail.com>
2022-03-22 18:56:28 +0100

    on avance

--- terre12.swift
+++ terre12.swift
@@ -1,3 +1,23 @@
 // 12 to 24
 
-// Cette fois ci essaye de faire avec la méthode où c'est pas trois arguments séparés mais un seul
+var argument = CommandLine.arguments
+argument.remove(at:0)
+
+var heure = argument[0]
+var colons = argument[1]
+var minute = argument[2]
+var pmOrAm = argument[3]
+
+if argument.count != 4 {
+    print ("Vous devez fournir l'heure en respectant cette forme : 23 : 43 PM, c'est à dire mettre des espaces entre chaque éléments ")
+} else if ((Int(heure) == nil) || (Int(minute) == nil )) {
+    print ("Vous devez fournir l'heure en respectant cette forme : 23 : 43 PM, c'est à dire mettre des espaces entre chaque éléments ")
+} else if Int(heure)! > 12 || Int(minute)! > 60 {
+    print ("Il n'y a que 24 heure dans la journée et que 60 minutes dans l'heure et tu dois mettre l'heure dans un format de douze heures")
+} else if pmOrAm != "AM" && pmOrAm != "PM" {
+    print ("précise AM ou PM mon ami !")
+} else {
+    switch Int(heure)! {
+    case 
+    }
+}
