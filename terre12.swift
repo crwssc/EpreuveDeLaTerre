e18169c041f70e8a747fe808d9928972b7ba986e
croissance <myadressemailisthat@gmail.com>
2022-04-01 16:26:08 +0200

    histoire des heures résolue

--- terre12.swift
+++ terre12.swift
@@ -12,12 +12,19 @@ if argument.count != 4 {
     print ("Vous devez fournir l'heure en respectant cette forme : 23 : 43 PM, c'est à dire mettre des espaces entre chaque éléments ")
 } else if ((Int(heure) == nil) || (Int(minute) == nil )) {
     print ("Vous devez fournir l'heure en respectant cette forme : 23 : 43 PM, c'est à dire mettre des espaces entre chaque éléments ")
-} else if Int(heure)! > 12 || Int(minute)! > 60 {
+} else if Int(heure)! > 12 || Int(minute)! > 60 {
     print ("Il n'y a que 24 heure dans la journée et que 60 minutes dans l'heure et tu dois mettre l'heure dans un format de douze heures")
 } else if pmOrAm != "AM" && pmOrAm != "PM" {
     print ("précise AM ou PM mon ami !")
 } else {
-    switch Int(heure)! {
-    case 
+    switch pmOrAm {
+    case "PM":
+        if Int(heure) == 12 {
+            print("00:\(minute)")
+        } else {
+            print("\(2 * Int(heure)!):\(minute)")
+        }
+    default:
+        print("\(heure):\(minute)")
     }
 }
