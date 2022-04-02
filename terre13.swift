8291b2887560dca723619dd852961cd67b45cca4
croissance <myadressemailisthat@gmail.com>
2022-04-02 15:10:26 +0200

    proche de trouver

--- terre13.swift
+++ terre13.swift
@@ -5,14 +5,20 @@ argument.remove(at:0)
 
 if argument.count != 3 {
     print ("Vous devez fournir exactement trois nombres entiers")
-} else if (Int(argument[0])! == nil || Int(argument[1])! == nil || Int(argument[0])! == nil) {
+} else if (Int(argument[0]) == nil || Int(argument[1]) == nil || Int(argument[0]) == nil) {
     print ("Vous devez fournir des nombres entiers")
 } else {
-    if (argument[0] < argument[1] < argument[2]) == true {
+    if (argument[0] < argument[1]) && (argument[1] < argument[2]) == true {
         print (argument[1])
-    } else if (argument[1] < argument[0] < argument[2]) == true {
+    } else if (argument[2] < argument[0]) && (argument[0] < argument[1]) == true {
         print (argument[0])
-    } else {
+    } else if (argument[1] < argument[2]) && (argument[2] < argument[0]) == true {
+        print (argument[2])
+    } else if (argument[0] > argument[1]) && (argument[1] > argument[2]) == true {
+        print (argument[1])
+    } else if (argument[2] > argument[0]) && (argument[0] > argument[1]) == true {
+        print (argument[0])
+    } else if (argument[1] > argument[2]) && (argument[2] > argument[0]) == true {
         print (argument[2])
     }
 }
