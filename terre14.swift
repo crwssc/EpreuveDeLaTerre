69f84ebbd62b2f4f4a900f27b7fbd059de8a81c0
croissance <myadressemailisthat@gmail.com>
2022-04-12 10:50:21 +0200

    petit à petit

--- terre14.swift
+++ terre14.swift
@@ -6,8 +6,8 @@ argument.remove(at:0)
 if argument.count < 1 {
     print ("Vous devez fournir au moins un argument")
 } else {
-    for i in 1...argument.count {
-        if > = true {
+    for i in 0...argument.count {
+        if argument[i] < argument[i+1] {
             print ("Triée !")
         } else {
             print ("Pas Triée !")
